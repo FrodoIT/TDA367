0a90e9c1b1f282ebc9fde6b004fb6d48ced354e2
Ivar Josefsson <ivar_busig@hotmail.com>
2014-03-26 14:12:05 +0100

    Added test for player.move

--- TisButAScratch/src/test/java/PlayerTest.java
+++ TisButAScratch/src/test/java/PlayerTest.java
@@ -6,10 +6,15 @@ import org.junit.Test;
 
 
 public class PlayerTest {
-
+	Player player;
+	
+	@Before
+	public void before() {
+		player = new Player();
+	}
+	
 	@Test
 	public void testPlayer() {
-		Player player = new Player();
 	}
 
 	@Test
@@ -19,23 +24,34 @@ public class PlayerTest {
 	
 	@Test
 	public void testGetPos() {
-		Player player = new Player(new Point (25, 25));
-		assertTrue(player.getPos().equals(new Point(25, 25)));
-		player = new Player();
 		assertTrue(player.equals(new Point(10, 10)));
+		Player player = new Player(new Point (25, 25));
+		assertTrue(player.getPos().equals(new Point(25, 25)));		
 	}
 	
 	@Test
 	public void testGetDamage() {
-		Player player = new Player();
 		assertTrue(player.getDamage() == 2);
 	}
 	
 	@Test
 	public void testGetRange() {
-		Player player = new Player();
 		assertTrue(player.getRange() == 1);
 	}
+	
+	@Test
+	public void testMove() {
+		player.move(Direction.SOUTH);
+		assertTrue(player.getPos.equals(new Point(10, 20)));
+		player.move(Direction.EAST);
+		assertTrue(player.getPos.equals(new Point(20, 20)));
+		player.move(Direction.NORTH);
+		assertTrue(player.getPos.equals(new Point(20, 10)));
+		player.move(Direction.WEST);
+		assertTrue(player.getPos.equals(new Point(10, 10)));
+	}
+	
+	
 	
 	
 }
