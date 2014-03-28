bd3024deb7c165336e729c7c899ce41293f34941
Ivar Josefsson <ivar_busig@hotmail.com>
2014-03-28 14:28:23 +0100

    Added testsuite for RoomTest

---
+++ TisButAScratch/src/test/java/RoomTest.java
@@ -0,0 +1,43 @@
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.awt.Point;
+
+import model.Direction;
+import model.Room;
+
+import org.junit.Test;
+
+/**
+ * 
+ * @author Ivar Cannonbait Josefsson
+ *
+ */
+public class RoomTest {
+
+	@Test
+	public void testRoom() {
+		Room room = new Room();
+		assertTrue(room.getPlayerPosition().equals(new Point(320, 240)));
+	}
+
+	@Test
+	public void testIsCompleted() {
+		fail("Not yet implemented");
+	}
+
+
+	@Test
+	public void testGetPlayerPosition() {
+		Room room = new Room();
+		assertTrue(room.getPlayerPosition().equals(new Point(320, 240)));
+	}
+
+	@Test
+	public void testUpdate() {
+		Room room = new Room();
+		room.update(Direction.SOUTH);
+		assertTrue(room.getPlayerPosition().equals(new Point(320, 242)));
+	}
+
+}
