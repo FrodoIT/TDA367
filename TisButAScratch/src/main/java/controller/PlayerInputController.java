720c5a355c7c94de0c6c68cd7c888878891929c4
Alma Ottedag <pippin@Pippinizer.(none)>
2014-03-28 15:20:31 +0100

    Added PlayerInputController with update method for the model.

---
+++ TisButAScratch/src/main/java/controller/PlayerInputController.java
@@ -0,0 +1,51 @@
+package controller;
+
+import model.Model;
+import model.PlayerCommand;
+
+import org.newdawn.slick.Input;
+
+public class PlayerInputController {
+	private Model model;
+
+
+	public PlayerInputController(Model model){
+		this.model = model;
+	}
+
+	public void update(Input input) {
+
+		if(input.isKeyDown(208) && input.isKeyDown(203)){
+			//Move South-west
+			model.recievePlayerInput(PlayerCommand.GO_SOUTHWEST);
+		} else if(input.isKeyDown(208) && input.isKeyDown(205)){
+			//Move South-east
+			model.recievePlayerInput(PlayerCommand.GO_SOUTHEAST);
+		} else if(input.isKeyDown(200) && input.isKeyDown(203)){
+			//Move North-west
+			model.recievePlayerInput(PlayerCommand.GO_NORTHWEST);
+			
+		} else if(input.isKeyDown(200) && input.isKeyDown(205)){
+			//Move North-east
+			model.recievePlayerInput(PlayerCommand.GO_NORTHEAST);
+			
+		} else if(input.isKeyDown(208)){
+			//Move South
+			model.recievePlayerInput(PlayerCommand.GO_SOUTH);
+			
+		} else if(input.isKeyDown(200)){
+			//Move North
+			model.recievePlayerInput(PlayerCommand.GO_NORTH);
+			
+		} else if(input.isKeyDown(203)){
+			//Move West
+			model.recievePlayerInput(PlayerCommand.GO_WEST);
+		} else if(input.isKeyDown(205)){
+			//Move East
+			model.recievePlayerInput(PlayerCommand.GO_EAST);
+		} 
+		//TODO: implement interact and attack
+
+	}
+
+}
