42bae98cfe0bbd79b9b79e5050faf83b691bf064
Alma Ottedag <alma.ottedag@gmail.com>
2014-03-29 15:08:59 +0100

    Added input reading connection for attack and interact in PlayerInputController.

--- TisButAScratch/src/main/java/controller/PlayerInputController.java
+++ TisButAScratch/src/main/java/controller/PlayerInputController.java
@@ -24,27 +24,32 @@ public class PlayerInputController {
 		} else if(input.isKeyDown(200) && input.isKeyDown(203)){
 			//Move North-west
 			model.recievePlayerInput(PlayerCommand.GO_NORTHWEST);
-			
+
 		} else if(input.isKeyDown(200) && input.isKeyDown(205)){
 			//Move North-east
 			model.recievePlayerInput(PlayerCommand.GO_NORTHEAST);
-			
+
 		} else if(input.isKeyDown(208)){
 			//Move South
 			model.recievePlayerInput(PlayerCommand.GO_SOUTH);
-			
+
 		} else if(input.isKeyDown(200)){
 			//Move North
 			model.recievePlayerInput(PlayerCommand.GO_NORTH);
-			
+
 		} else if(input.isKeyDown(203)){
 			//Move West
 			model.recievePlayerInput(PlayerCommand.GO_WEST);
 		} else if(input.isKeyDown(205)){
 			//Move East
 			model.recievePlayerInput(PlayerCommand.GO_EAST);
-		} 
-		//TODO: implement interact and attack
+		} else if(input.isKeyDown(44)){
+			model.recievePlayerInput(PlayerCommand.INTERACT);
+			//Z for interact.
+		} else if(input.isKeyDown(45)){
+			model.recievePlayerInput(PlayerCommand.ATTACK);
+			//X for attack
+		}
 
 	}
 
