7378232a39dc23c9c4de5ea8f65ccd87d24e0510
André Samuelsson <tejpbit@hotmail.se>
2014-03-29 16:13:16 +0100

    Fixed constants in PlayerInputController

--- TisButAScratch/src/main/java/controller/PlayerInputController.java
+++ TisButAScratch/src/main/java/controller/PlayerInputController.java
@@ -15,40 +15,40 @@ public class PlayerInputController {
 
 	public void update(Input input) {
 
-		if(input.isKeyDown(208) && input.isKeyDown(203)){
+		if(input.isKeyDown(Input.KEY_DOWN) && input.isKeyDown(Input.KEY_LEFT)){
 			//Move South-west
 			model.queuePlayerInput(PlayerCommand.GO_SOUTHWEST);
-		} else if(input.isKeyDown(208) && input.isKeyDown(205)){
+		} else if(input.isKeyDown(Input.KEY_DOWN) && input.isKeyDown(Input.KEY_RIGHT)){
 			//Move South-east
 			model.queuePlayerInput(PlayerCommand.GO_SOUTHEAST);
-		} else if(input.isKeyDown(200) && input.isKeyDown(203)){
+		} else if(input.isKeyDown(Input.KEY_UP) && input.isKeyDown(Input.KEY_LEFT)){
 			//Move North-west
 			model.queuePlayerInput(PlayerCommand.GO_NORTHWEST);
 
-		} else if(input.isKeyDown(200) && input.isKeyDown(205)){
+		} else if(input.isKeyDown(Input.KEY_UP) && input.isKeyDown(Input.KEY_RIGHT)){
 			//Move North-east
 			model.queuePlayerInput(PlayerCommand.GO_NORTHEAST);
 
-		} else if(input.isKeyDown(208)){
+		} else if(input.isKeyDown(Input.KEY_DOWN)){
 			//Move South
 			model.queuePlayerInput(PlayerCommand.GO_SOUTH);
 
-		} else if(input.isKeyDown(200)){
+		} else if(input.isKeyDown(Input.KEY_UP)){
 			//Move North
 			model.queuePlayerInput(PlayerCommand.GO_NORTH);
 
-		} else if(input.isKeyDown(203)){
+		} else if(input.isKeyDown(Input.KEY_LEFT)){
 			//Move West
 			model.queuePlayerInput(PlayerCommand.GO_WEST);
-		} else if(input.isKeyDown(205)){
+		} else if(input.isKeyDown(Input.KEY_RIGHT)){
 			//Move East
 			model.queuePlayerInput(PlayerCommand.GO_EAST);
 		}
 		//TODO: Should be possible to attack at the same time as moving.
-		else if(input.isKeyDown(44)){
+		else if(input.isKeyDown(Input.KEY_Z)){
 			model.queuePlayerInput(PlayerCommand.INTERACT);
 			//Z for interact.
-		} else if(input.isKeyDown(45)){
+		} else if(input.isKeyDown(Input.KEY_X)){
 			model.queuePlayerInput(PlayerCommand.ATTACK);
 			//X for attack
 		}
