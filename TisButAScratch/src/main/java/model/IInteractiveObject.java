c2957fa188e640ce7ba3c96d1412b60f192f554b
Alma Ottedag <alma.ottedag@gmail.com>
2014-03-29 19:16:15 +0100

    renamed IObject to IInteractiveObject

---
+++ TisButAScratch/src/main/java/model/IInteractiveObject.java
@@ -0,0 +1,11 @@
+package model;
+import java.awt.Point;
+
+/**
+ * An interface for all interactive objects.
+ * @author pippin
+ *
+ */
+public interface IInteractiveObject {
+	public Point getPosition();
+}
