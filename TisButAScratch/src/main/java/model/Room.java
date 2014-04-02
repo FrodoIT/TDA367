e7ae4ee90e07c17b4479a86001b09e88821ca237
Ivar Josefsson <ivar_busig@hotmail.com>
2014-04-02 15:08:05 +0200

    Added Room class fixture

--- TisButAScratch/src/main/java/model/Room.java
+++ TisButAScratch/src/main/java/model/Room.java
@@ -1,5 +1,17 @@
 package model;
 
 public class Room {
+	int[][] collisionMap;
+	Player player;
+	
+	public void enterRoom(Player player){
+		//TODO
+	}
+	
+	public void leaveRoom(Player player){
+		//TODO
+	}
+	
+	
 
 }
