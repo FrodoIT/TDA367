fcf7d4ca328cc320c75fcf79972126e24bed2db6
Ivar Josefsson <ivar_busig@hotmail.com>
2014-04-02 15:07:54 +0200

    added PlayerInput interface

--- TisButAScratch/src/main/java/model/PlayerInput.java
+++ TisButAScratch/src/main/java/model/PlayerInput.java
@@ -1,5 +1,10 @@
 package model;
 
 public interface PlayerInput {
-
+	public boolean getAttackInput();
+	public boolean getInteractInput();
+	public MoveDirection getMoveInput();
+	public void setAttackInput(boolean attack);
+	public void setInteractInput(boolean interact);
+	public void setMoveInput(MoveDirection direction);
 }
