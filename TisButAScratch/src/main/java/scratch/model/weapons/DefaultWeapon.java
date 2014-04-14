3938b79acb7413f6eca7c018811c61fee719a419
Alma Ottedag <alma.ottedag@gmail.com>
2014-04-14 15:49:10 +0200

    refactored and fixed indentation of Defaultweapon. Especially added 'final' to stuff

--- TisButAScratch/src/main/java/scratch/model/weapons/DefaultWeapon.java
+++ TisButAScratch/src/main/java/scratch/model/weapons/DefaultWeapon.java
@@ -2,7 +2,6 @@ package scratch.model.weapons;
 
 import java.awt.geom.Rectangle2D;
 import java.util.Date;
-import scratch.model.Constants;
 
 /**
  * The weapon DefaultWeapon:
@@ -11,48 +10,53 @@ import scratch.model.Constants;
  Range = 1
  * @author Alma Ottedag
  */
-public class DefaultWeapon implements IWeapon {
-	private final int damage;
-	private final int range;
-        private final Rectangle2D.Double attackArea;
-        //Minimum time between attacks in milliseconds
-        private final int attackInterval;
-        private Date lastAttack;
+public final class DefaultWeapon implements IWeapon {
+    private final int damage;
+    private final int range;
+    private final Rectangle2D.Double attackArea;
+    //Minimum time between attacks in milliseconds
+    private final int attackInterval;
+    private Date lastAttack;
+    private Date thisMoment;
 
-	public DefaultWeapon(){
-		damage = 2;
-		range = 1;
-                attackArea = new Rectangle2D.Double(0, 0, 3, 3);
-                lastAttack = new Date();
-                attackInterval = 200;
-	}
-	@Override
-	public int getDamage() {
-		return damage;
-	}
+    public DefaultWeapon(){
+        damage = 2;
+        range = 1;
+        attackArea = new Rectangle2D.Double(0, 0, 3, 3);
+        lastAttack = new Date();
+        attackInterval = 200;
+    }
+    @Override
+    public int getDamage() {
+        return damage;
+    }
 
-	@Override
-	public int getRange() {
-		return range;
-	}
-        
-        @Override
-        public Rectangle2D.Double getAttackArea(){
-            return (Rectangle2D.Double)attackArea.clone();
+    @Override
+    public int getRange() {
+        return range;
+    }
+
+    @Override
+    public Rectangle2D.Double getAttackArea(){
+        return (Rectangle2D.Double)attackArea.clone();
+    }
+
+    /**
+     * Tell the weapon to execute an attack
+     * @return true if attack was done
+     */
+    @Override
+    public boolean attack(){
+        if (hasCooledDown()){
+            lastAttack = thisMoment;
+            return true;
         }
-        
-        /**
-         * Tell the weapon to execute an attack
-         * @return true if attack was done
-         */
-        @Override
-        public boolean attack(){
-            Date moment = new Date();
-            if (Math.abs(lastAttack.getTime() - moment.getTime()) > attackInterval){
-                lastAttack = moment;
-                return true;
-            }
-            return false;
-        }
+        return false;
+    }
+
+    private boolean hasCooledDown(){
+        thisMoment = new Date();
+        return Math.abs(lastAttack.getTime() - thisMoment.getTime()) > attackInterval;
+    }
 
 }
