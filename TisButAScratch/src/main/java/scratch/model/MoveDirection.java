9855214014b61728194fe9e74dc787cbb3f9092f
Anna Nylander <Anna.chiakichama@gmail.com>
2014-04-16 22:26:38 +0200

    Added constuctor and getters and coresponding values to each enumvalue

--- TisButAScratch/src/main/java/scratch/model/MoveDirection.java
+++ TisButAScratch/src/main/java/scratch/model/MoveDirection.java
@@ -4,5 +4,20 @@ package scratch.model;
  * revised Ivar Josefsson 2014-03-29
  */
 public enum MoveDirection {
-	NORTH, SOUTH, WEST, EAST, NORTHEAST, NORTHWEST, SOUTHEAST, SOUTHWEST, NONE;
+	NORTH(0, -1), SOUTH(0, 1), WEST(-1,0), EAST(1,0), NORTHEAST(1,-1), NORTHWEST(-1,-1), SOUTHEAST(1,1), SOUTHWEST(1,-1), NONE(0,0);
+
+	private int x, y;
+
+	MoveDirection(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public int getX(){
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
 }
