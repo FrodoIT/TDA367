ea2993a71cece6a1cc05478f44d6e95ac67b379a
Alma Ottedag <alma.ottedag@gmail.com>
2014-04-30 19:24:10 +0200

    Added root directory for tests, as well as a dummy test for interactive object type

---
+++ TisButAScratch/src/test/java/scratch/model/InteractiveObjectTypeTest.java
@@ -0,0 +1,32 @@
+package scratch.model;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+/**
+ * Testclass for Interactive Object Type
+ */
+public class InteractiveObjectTypeTest extends TestCase {
+    @Test
+    public void setUp() throws Exception {
+        super.setUp();
+
+    }
+    @Test
+    public void testInteract() throws Exception {
+
+    }
+
+    @Test
+    public void testCreateCopy() throws Exception {
+
+    }
+    @Test
+    public void testGetArea() throws Exception {
+
+    }
+    @Test
+    public void testGetID() throws Exception {
+
+    }
+}
