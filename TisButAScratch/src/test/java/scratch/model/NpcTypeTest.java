f4a9de7f89ddfcce283988af481af12d511ba3de
Anna Nylander <Anna.chiakichama@gmail.com>
2014-05-05 23:10:13 +0200

    Tests for Room and NPCType edited, they are not yet done, but I shouldn't make too big commits and I might need some help.

--- TisButAScratch/src/test/java/scratch/model/NpcTypeTest.java
+++ TisButAScratch/src/test/java/scratch/model/NpcTypeTest.java
@@ -1,13 +1,25 @@
 package scratch.model;
 
+import com.google.inject.Guice;
+import com.google.inject.Injector;
 import junit.framework.TestCase;
+import scratch.model.mockModules.MockModule;
+import scratch.model.weapons.DefaultWeapon;
+import scratch.model.weapons.IWeapon;
+
+import java.awt.geom.Rectangle2D;
 
 /**
  * Created by Anna on 2014-05-02.
  */
 public class NpcTypeTest extends TestCase {
+	private NpcType npcType;
+	private INPCMove NPCMove;
 	public void setUp() throws Exception {
-		super.setUp();
+		Injector injector = Guice.createInjector(new MockModule());
+		IWeapon weapon= injector.getInstance(IWeapon.class);
+		INPCMove NPCMove = injector.getInstance(INPCMove.class);
+		npcType = new NpcType(new Rectangle2D.Double(32,32,32,32), weapon, 1, 1, "/res/playerSprite.tmx", 0, NPCMove);
 
 	}
 
--- TisButAScratch/src/test/java/scratch/model/RoomTest.java
+++ TisButAScratch/src/test/java/scratch/model/RoomTest.java
@@ -3,6 +3,8 @@ package scratch.model;
 import com.google.inject.*;
 import junit.framework.TestCase;
 import org.junit.Before;
+import org.junit.Test;
+import scratch.model.mockModules.MockModule;
 
 import java.awt.geom.Rectangle2D;
 
@@ -15,7 +17,7 @@ public class RoomTest extends TestCase {
 
 	@Before
 	public void setUp() {
-		Injector injector = Guice.createInjector();
+		Injector injector = Guice.createInjector(new MockModule());
 		map = injector.getInstance(IMap.class);
 		room= new Room(map);
 	}
@@ -24,7 +26,32 @@ public class RoomTest extends TestCase {
 
 	}
 
+	//made to create mock-players used in tests
+	private Player createPlayerForTest(int id) {
+		Injector injector = Guice.createInjector(new MockModule());
+		IPlayerInput playerInput = injector.getInstance(IPlayerInput.class);
+		return new Player(playerInput, new Rectangle2D.Double(0,0,32,32),id);
+	}
+
 	public void testUpdate() throws Exception {
+
+	}
+
+	public void testUpdateCharater(ICharacter character) throws Exception {
+
+	}
+	//the function MapCollision is used in AllowedPosition so I'm just testing one of them.
+	public void testAllowedPosition(Rectangle2D.Double unitTile, Vector2D toPosition){
+		Player player1 = createPlayerForTest(0);
+		room.enterRoom(player1);
+		//not really sure of how to test this. Might need help with this.
+	}
+
+	public void testMapCollision(Rectangle2D.Double objectToPlace, Vector2D placeToPut){
+
+	}
+
+	public void testDealDamage(){
 
 	}
 
@@ -37,25 +64,31 @@ public class RoomTest extends TestCase {
 		assertTrue(room.getPlayers().contains(player1) && room.getPlayers().contains(player2));
 	}
 
-	private Player createPlayerForTest(int id) {
-		Injector injector = Guice.createInjector();
-		IPlayerInput playerInput = injector.getInstance(IPlayerInput.class);
-		return new Player(playerInput, new Rectangle2D.Double(0,0,32,32),id);
-	}
-
 	public void testExitRoom() throws Exception {
-
+		Player player1 = createPlayerForTest(0);
+		Player player2 = createPlayerForTest(1);
+		room.enterRoom(player1);
+		room.enterRoom(player2);
+		assertTrue(!room.getPlayers().isEmpty());
+		room.exitRoom(player1);
+		assertTrue(!room.getPlayers().contains(player1) && room.getPlayers().get(0).getID()==1);
+		assertEquals(1,room.getPlayers().get(0).getID());
+		room.exitRoom(player2);
+		assertTrue(room.getPlayers().isEmpty());
 	}
 
+	@Test(expected=NullPointerException.class)
+	public void testExitRoomException() throws Exception {
+		Player player = createPlayerForTest(0);
+		room.exitRoom(player);
+		assertTrue(room.getPlayers().isEmpty());
+	}
+	//I dont think I need to test this since I use it i so many other tests.
 	public void testGetPlayers() throws Exception {
-
 	}
 
 	public void testGetNpcs() throws Exception {
-
+		assertEquals(1,room.getNpcs().size()); //change 1 to expected number of npcs expected on the specific map.
 	}
 
-	public void testGetMap() throws Exception {
-
-	}
 }
