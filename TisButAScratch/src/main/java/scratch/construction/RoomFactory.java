ef1fa1bf527021f0ca6f8d839d3ae5ef6f34d5fb
Alma Ottedag <alma.ottedag@gmail.com>
2014-05-06 21:03:45 +0200

    Refactored and further implemented the Room Factory

--- TisButAScratch/src/main/java/scratch/construction/RoomFactory.java
+++ TisButAScratch/src/main/java/scratch/construction/RoomFactory.java
@@ -2,47 +2,75 @@ package scratch.construction;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
+import com.google.inject.internal.cglib.proxy.$MethodInterceptor;
+import net.java.games.util.plugins.Plugin;
 import scratch.model.Room;
 
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.tiled.TiledMap;
+import scratch.model.Vector2D;
 
 /**
- * This class is only temporaray so we can load a map into the game.
+ * The Room factory is the main factory for creating rooms. It uses all sub-factories to store
+ * objects and npcs in the given room, which are in turn loaded from the given TiledMap.
  * @author André Samuelsson
  *
  */
 public final class RoomFactory {
-	
-	private TiledMap map;
-	private final Room room;
-	
-	public RoomFactory() {
-		try {
-			map = new TiledMap("res/untitled.tmx");
-		} catch (SlickException e) {
-			e.printStackTrace();
-		}
-		
-		this.room = new Room(new SlickMap(map));
-	}
-	
-	public TiledMap getTiledMap() {
-		
-		return map;
-	}
-        
-        	
-	public List<Room> getRooms() {
-		
-		List<Room> rooms = new ArrayList<Room>();
-		rooms.add(room);
-		return rooms;
-	}
-	
-	public Room getRoom() {
-		
-		return room;
-	}
+
+    private TiledMap map;
+    private List<Room> rooms;
+    private List<PluginUserFactory> pluginUserFactories;
+    private SlickMap slickMap;
+
+
+    public RoomFactory() {
+        try {
+            map = new TiledMap("res/untitled.tmx");
+        } catch (SlickException e) {
+            e.printStackTrace();
+        }
+
+        addSubFactories();
+
+        slickMap = new SlickMap(map);
+        rooms = new ArrayList<Room>();
+        addRooms();
+
+    }
+
+    public void initialiseRoom(Room room){
+
+    }
+
+    private void addRooms() {
+        //TODO: This should be further extended when several rooms are implemented.
+        rooms.add(new Room(slickMap));
+    }
+
+    private void addSubFactories() {
+        pluginUserFactories = new ArrayList<PluginUserFactory>();
+        pluginUserFactories.add(new NpcFactory());
+        pluginUserFactories.add(new InteractiveObjectFactory());
+    }
+
+    public TiledMap getTiledMap() {
+
+        return map;
+    }
+
+    public TiledMap getMap() {
+        return map;
+    }
+
+    public List<PluginUserFactory> getPluginUserFactories() {
+        return pluginUserFactories;
+    }
+
+    public List<Room> getRooms() {
+        return rooms;
+    }
 }
