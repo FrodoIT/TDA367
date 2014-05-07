e95558fa712b9c21e31887bc275c8fab9a5c17f0
Ivar Josefsson <ivar_busig@hotmail.com>
2014-05-07 16:26:01 +0200

    Added default usage of id 0 view for NpcTypes with no found NpcView

--- TisButAScratch/src/main/java/scratch/view/RoomView.java
+++ TisButAScratch/src/main/java/scratch/view/RoomView.java
@@ -29,11 +29,14 @@ public class RoomView {
 	public void render(GameContainer c, Graphics g) {
 		map.render(0, 0);
                 
-
+                
                 for(Map.Entry<Integer, NpcType> npcEntry : room.getNpcs().entrySet()){
+                    
                     int id = npcEntry.getValue().getId();
                     if(npcViews.containsKey(id)){
                         npcViews.get(id).render(npcEntry.getValue(), g);
+                    } else {
+                        npcViews.get(0).render(npcEntry.getValue(), g);
                     }
                 }
 	}
