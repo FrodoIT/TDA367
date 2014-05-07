571a76bd72c64d841f0539c3c1510ca6a601c4ff
Anna Nylander <Anna.chiakichama@gmail.com>
2014-05-07 11:23:33 +0200

    npc's attackArea is now shown with blue on the map

--- TisButAScratch/src/main/java/scratch/view/NpcView.java
+++ TisButAScratch/src/main/java/scratch/view/NpcView.java
@@ -6,10 +6,14 @@
 
 package scratch.view;
 
+import org.newdawn.slick.Color;
+import org.newdawn.slick.geom.Rectangle;
 import scratch.model.INpc;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.tiled.TiledMap;
+
+import java.awt.geom.Rectangle2D;
 
 /**
  *
@@ -17,6 +21,7 @@ import org.newdawn.slick.tiled.TiledMap;
  */
 public class NpcView{
     private SpriteDirectionRenderer spriteHandler;
+	private Rectangle2D.Double attackArea;
 
     public NpcView(String imagePath){
         try {
@@ -28,6 +33,14 @@ public class NpcView{
 
     public void render(INpc npc, Graphics g){
 	    if(npc.isAlive()) {
+		    if(!npc.weaponHasCooledDown()) {
+			    //saves attackArea every time player fights co be able to continue to render it until the weaponCD is down.
+			    if(npc.isAttacking()) {
+				    attackArea = npc.getAttackArea();
+			    }
+			    g.setColor(Color.blue);
+			    g.fill(new Rectangle((int) attackArea.getX(), (int) attackArea.getY(), (int) attackArea.getWidth(), (int) attackArea.getHeight()));
+		    }
 		    spriteHandler.render(g, npc.getMoveDirection(), npc.getPosition().getX(), npc.getPosition().getY());
 	    }
 
