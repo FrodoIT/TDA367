b9ad815c8e9aad26831a44b7d8701d1f96ad3c02
Alma Ottedag <alma.ottedag@gmail.com>
2014-05-07 11:11:49 +0200

    Finalised the RoomTest

--- TisButAScratch/src/test/java/scratch/model/RoomTest.java
+++ TisButAScratch/src/test/java/scratch/model/RoomTest.java
@@ -36,31 +36,14 @@ public class RoomTest extends TestCase {
 		return new Player(playerInput, new Rectangle2D.Double(0,0,32,32),id);
 	}
 
-	public void testUpdate() throws Exception {
+	@Test
+    public void testUpdate() throws Exception {
         room.enterRoom(createPlayerForTest(1));
         room.update();
         assert(room.isUpdatingNpcs());
         assert(room.isUpdatingPlayers());
 	}
-
-	public void testUpdateCharater(ICharacter character) throws Exception {
-
-	}
-	//the function MapCollision is used in AllowedPosition so I'm just testing one of them.
-	public void testAllowedPosition(Rectangle2D.Double unitTile, Vector2D toPosition){
-		Player player1 = createPlayerForTest(0);
-		room.enterRoom(player1);
-		//not really sure of how to test this. Might need help with this.
-	}
-
-	public void testMapCollision(Rectangle2D.Double objectToPlace, Vector2D placeToPut){
-
-	}
-
-	public void testDealDamage(){
-
-	}
-
+    @Test
 	public void testEnterRoom() throws Exception {
 		assertTrue(room.getPlayers().isEmpty());
 		Player player1 = createPlayerForTest(0);
@@ -69,7 +52,7 @@ public class RoomTest extends TestCase {
 		room.enterRoom(player2);
 		assertTrue(room.getPlayers().contains(player1) && room.getPlayers().contains(player2));
 	}
-
+    @Test
 	public void testExitRoom() throws Exception {
 		Player player1 = createPlayerForTest(0);
 		Player player2 = createPlayerForTest(1);
