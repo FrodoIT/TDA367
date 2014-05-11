e64d704df5c864b0cd9ee5999dbd8071d416f53a
Anna Nylander <Anna.chiakichama@gmail.com>
2014-05-11 16:33:08 +0200

    Added arguments to the method initializing NPC. now we just need info from the map what kind of npc and where to put it and xml will solve the rest

--- TisButAScratch/src/main/java/scratch/construction/NpcFactory.java
+++ TisButAScratch/src/main/java/scratch/construction/NpcFactory.java
@@ -6,15 +6,16 @@ import scratch.construction.plugin.PluginConstants;
 import scratch.construction.plugin.exported.SimpleNPCPlugin;
 import scratch.model.IMap;
 import scratch.model.NpcType;
+import scratch.model.Vector2D;
 import scratch.model.weapons.DefaultWeapon;
 
+import java.awt.*;
+import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
 import java.io.File;
 import java.util.Map;
 
 public final class NpcFactory extends PluginUserFactory<NpcType> {
-
-
 
     public static final String KEY = "npc_factory";
 
@@ -34,16 +35,27 @@ public final class NpcFactory extends PluginUserFactory<NpcType> {
                 keyToConstant = PluginConstants.BOX;
             }
           //  super.getGivenTypeMap().put(keyToConstant, new NpcType(entry.getValue(), new DefaultWeapon(), 10, 2, "monster.tmx", 10, new SimpleNPCPlugin()));
-			super.getGivenTypeMap().put(keyToConstant,NPCXML());
+			super.getGivenTypeMap().put(keyToConstant,NPCXML("StandardEnemy", new Vector2D(500,400)));
+
         }
 
     }
-	private NpcType NPCXML(){
+
+	/**
+	 * Method for reading an xml file. If creating new xml, please follow the structure in StanardEnemy.xml and add to res-map.
+	 * If new weapon/NPCMOVEplugin is created you will need to add them to the respective transformer class.
+	 * @param file the name of the xmlfile without extension, eg "StandardEnemy" Do NOT add location.
+	 * @param position The position the npc should have
+	 * @return A npc with the attributes as in the xml file.
+	 */
+	private NpcType NPCXML(String file, Vector2D position){
 		Serializer serializer = new Persister(new MyMatcher());
-		File source = new File("res/NPCType.xml");
+		file= "res/"+file+".xml";
+		File source = new File(file);
 		try {
 			NpcType npc= serializer.read(NpcType.class, source);
 			System.out.println(npc.toString());
+			npc.setPosition(position);
 			return npc;
 		} catch (Exception e) {
 			System.out.println("XML-READING FAILED: " + e.toString());
