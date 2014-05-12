f4f6b6df30b6a25d3499faebe3f02820d8aa6d93
Ivar Josefsson <ivar_busig@hotmail.com>
2014-05-12 21:37:02 +0200

    Added several classes to register

--- TisButAScratch/src/main/java/scratch/network/Utilities.java
+++ TisButAScratch/src/main/java/scratch/network/Utilities.java
@@ -7,8 +7,12 @@
 package scratch.network;
 
 import com.esotericsoftware.kryo.Kryo;
-import scratch.model.Model;
-import scratch.model.Room;
+import java.util.HashMap;
+import java.util.HashSet;
+import scratch.construction.*;
+import scratch.model.*;
+import scratch.model.Character;
+import scratch.model.weapons.*;
 
 /**
  *
@@ -17,9 +21,43 @@ import scratch.model.Room;
 public class Utilities {
     public static void kryoRegister(Kryo kryo){
         kryo.register(PacketMessage.class);
-        //Looks ugly as shit, should be different solution
+        
+        //Looks ugly as shit, is there a different solution?
         kryo.register(PacketModel.class);
+        kryo.register(Character.class);
+        kryo.register(Constants.class);
+        kryo.register(DoorHandler.class);
+        kryo.register(IInteractiveObject.class);
+        kryo.register(IMap.class);
+        kryo.register(INPCMove.class);
+        kryo.register(IPlayerInput.class);
+        kryo.register(IRoomData.class);
         kryo.register(Model.class);
+        kryo.register(MoveDirection.class);
+        kryo.register(NpcType.class);
+        kryo.register(Player.class);
         kryo.register(Room.class);
+        kryo.register(Vector2D.class);
+        kryo.register(DefaultWeapon.class);
+        kryo.register(IWeapon.class);
+        kryo.register(HashMap.class);
+        kryo.register(HashSet.class);
+        
+        //Register constructionpackage
+        kryo.register(INPCMoveTransformer.class);
+        kryo.register(InteractiveObjectFactory.class);
+        kryo.register(MoveDirectionTransformer.class);
+        kryo.register(MyMatcher.class);
+        kryo.register(NpcFactory.class);
+        kryo.register(PluginUserFactory.class);
+        kryo.register(RectangleTransformer.class);
+        kryo.register(RoomDataTransformer.class);
+        kryo.register(RoomFactory.class);
+        kryo.register(SlickMap.class);
+        kryo.register(Test.class);
+        kryo.register(TiledMapPlus.class);
+        kryo.register(WeaponTranformer.class);
+        
+        
     }
 }
