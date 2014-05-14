242cc0ab376867faabcf2f47a6f17132d5271cb3
Anna Nylander <Anna.chiakichama@gmail.com>
2014-05-14 14:44:34 +0200

    test to make sure the movementpattern works as an acctual plugin.

---
+++ TisButAScratch/src/main/java/scratch/construction/plugin/exported/CPNPCPlugin.java
@@ -0,0 +1,50 @@
+package scratch.construction.plugin.exported;
+
+import com.google.inject.Inject;
+import scratch.construction.plugin.AIPlugin;
+import scratch.construction.plugin.Pluggable;
+import scratch.model.*;
+
+import java.awt.geom.Point2D;
+import java.util.Random;
+
+/**
+ * Created by Anna on 2014-05-14.
+ */
+@AIPlugin(id = 2)
+public final class CPNPCPlugin implements Pluggable<CPNPCPlugin>, INPCMove {
+	@Inject
+	private Random ran = new Random();
+	private IRoomData roomData;
+
+	@Override
+	public CPNPCPlugin get() {
+		return this;
+	}
+
+	@Override
+	public Vector2D calculateNewPosition(NpcType npc) {
+
+		return new Vector2D(ran.nextInt(3)-1, ran.nextInt(3)-1);
+	}
+
+	public boolean isAttacking(NpcType npc){
+		for(Player player: roomData.getPlayers()){
+			if(isWithinRange(player.getPosition(), npc.getPosition())){
+				return true;
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public void setRoomData(IRoomData roomData) {
+		this.roomData = roomData;
+	}
+
+	private boolean isWithinRange(Vector2D playerPos, Vector2D npcPos){
+		//+16 to get the center of the character, playerpos returns the point of the right high corner
+		Vector2D vector2D= new  Vector2D(new Point2D.Double(playerPos.getX()+16, playerPos.getY()+16), new Point2D.Double(npcPos.getX()+16, npcPos.getY()+16));
+		return(vector2D.getMagnitude()<= 48);
+	}
+}
