7a3faf46c54d8dbdba85e46a510cee461830a4c9
Alma Ottedag <alma.ottedag@gmail.com>
2014-05-14 14:16:05 +0200

    Fixed findbugs ussues in NpcFactory. However, all internal string definitions of npcs will have to be removed

--- TisButAScratch/src/main/java/scratch/construction/NpcFactory.java
+++ TisButAScratch/src/main/java/scratch/construction/NpcFactory.java
@@ -13,7 +13,8 @@ import java.util.Map;
 public final class NpcFactory extends PluginUserFactory<NpcType> {
 
     public static final String KEY = "npc_factory";
-
+    public static final String BASICMONSTER = "basicMonster";
+    public static final String SPECIALMONSTER = "specialMonster";
     private Room room;
 
     public NpcFactory(IMap map, Room room){
@@ -27,46 +28,44 @@ public final class NpcFactory extends PluginUserFactory<NpcType> {
         //TODO: This will extend when we add MORE types of NPCS.
         for(Map.Entry<String, Rectangle2D.Double> entry : super.getMap().getNpcRectangleMap().entrySet()){
             int keyToConstant = 0;
-            if(entry.getKey().equals("basicMonster")){
+            if(BASICMONSTER.compareTo(entry.getKey()) == 0){
                 keyToConstant = PluginConstants.DOOR;
-            } else if(entry.getKey().equals("specialMonster")){
+            } else if(SPECIALMONSTER.compareTo(entry.getKey()) == 0){
                 keyToConstant = PluginConstants.BOX;
             }
 
-            //TODO save NpcType returned from NPCXML should be saved. moveaistuffplugin should be added to it
-            //room should be sent to that moveaistuffplugin
+            //TODO save NpcType returned from should be saved. moveaistuffplugin should be added to it
             INPCMove movePattern = new SimpleNPCPlugin(); // this should be taken from loaded plugins not created here.
             movePattern.setRoomData(room);
-            NpcType loadedNpc = NPCXML("StandardEnemy", new Vector2D(500,400), 0);
+            final String standardEnemy = "StandardEnemy";
+            NpcType loadedNpc = npcXML(standardEnemy, new Vector2D(500, 400), 0);
             loadedNpc.setMovementPattern(movePattern);
             loadedNpc.registerListener(room);
 
-			super.getGivenTypeMap().put(keyToConstant, loadedNpc);
+            super.getGivenTypeMap().put(keyToConstant, loadedNpc);
 
         }
 
     }
 
-	/**
-	 * Method for reading an xml file. If creating new xml, please follow the structure in StanardEnemy.xml and add to res-map.
-	 * If new weapon/NPCMOVEplugin is created you will need to add them to the respective transformer class.
-	 * @param file the name of the xmlfile without extension, eg "StandardEnemy" Do NOT add location.
-	 * @param position The position the npc should have
-	 * @return A npc with the attributes as in the xml file.
-	 */
-	private NpcType NPCXML(String file, Vector2D position, int id){
-		Serializer serializer = new Persister(new MyMatcher());
-		file= "res/"+file+".xml";
-		File source = new File(file);
-		try {
-			NpcType npc= serializer.read(NpcType.class, source);
-			System.out.println(npc.toString());
-			npc.setPosition(position);
-			npc.setId(id);
-			return npc;
-		} catch (Exception e) {
-			System.out.println("XML-READING FAILED: " + e.toString());
-		}
-		return null;
-	}
+    /**
+     * Method for reading an xml file. If creating new xml, please follow the structure in StanardEnemy.xml and add to res-map.
+     * If new weapon/NPCMOVEplugin is created you will need to add them to the respective transformer class.
+     * @param file the name of the xmlfile without extension, eg "StandardEnemy" Do NOT add location.
+     * @param position The position the npc should have
+     * @return A npc with the attributes as in the xml file.
+     */
+    private NpcType npcXML(String file, Vector2D position, int id){
+        Serializer serializer = new Persister(new MyMatcher());
+        File source = new File("res/" + file + ".xml");
+        try {
+            NpcType npc= serializer.read(NpcType.class, source);
+            npc.setPosition(position);
+            npc.setId(id);
+            return npc;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
