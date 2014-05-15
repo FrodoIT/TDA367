952d98ba4c1df6d5e8d5ab9a5c3c698c065dd68c
André Samuelsson <tejpbit@hotmail.se>
2014-05-15 21:43:59 +0200

    pmd for CharacterChangeListener

--- TisButAScratch/src/main/java/scratch/model/CharacterChangeListener.java
+++ TisButAScratch/src/main/java/scratch/model/CharacterChangeListener.java
@@ -4,7 +4,7 @@ package scratch.model;
  * Created by pippin on 5/8/14.
  */
 public interface CharacterChangeListener {
-    public void handleCharacterMovement(AbstractCharacter character, Vector2D movement);
-    public void handleCharacterAttack(AbstractCharacter character);
-    public void handleCharacterInteraction(AbstractCharacter character);
+    void handleCharacterMovement(AbstractCharacter character, Vector2D movement);
+    void handleCharacterAttack(AbstractCharacter character);
+    void handleCharacterInteraction(AbstractCharacter character);
 }
