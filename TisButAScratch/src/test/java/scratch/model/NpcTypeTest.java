e9981fc0fb138136b909102cb07813c4ac693efd
André Samuelsson <tejpbit@hotmail.se>
2014-05-15 23:33:12 +0200

    pmd for NpcTypeTest

--- TisButAScratch/src/test/java/scratch/model/NpcTypeTest.java
+++ TisButAScratch/src/test/java/scratch/model/NpcTypeTest.java
@@ -3,6 +3,7 @@ package scratch.model;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import junit.framework.TestCase;
+import org.junit.Before;
 import org.junit.Test;
 import scratch.model.mockModules.MockModule;
 import scratch.model.weapons.IWeapon;
@@ -14,34 +15,35 @@ public class NpcTypeTest extends TestCase {
 	private NpcType npcType;
 
 
-	public void setUp() throws Exception {
-        Injector injector = Guice.createInjector(new MockModule());
-        IPlayerInput playerInput = injector.getInstance(IPlayerInput.class);
-		IWeapon weapon= injector.getInstance(IWeapon.class);
-        IMap map = injector.getInstance(IMap.class);
-        Player player = new Player(playerInput , new Rectangle2D.Double(50,50,32,32), 1, "/res/playerSprite.tmx");
-        INPCMove npcMove = injector.getInstance(INPCMove.class);
-        Room room = new Room(map, new DoorHandler());
+    @Before
+	public void setUp() {
+        final Injector injector = Guice.createInjector(new MockModule());
+        final IPlayerInput playerInput = injector.getInstance(IPlayerInput.class);
+        final IWeapon weapon= injector.getInstance(IWeapon.class);
+        final IMap map = injector.getInstance(IMap.class);
+        final Player player = new Player(playerInput , new Rectangle2D.Double(50,50,32,32), 1, "/res/playerSprite.tmx");
+        final INPCMove npcMove = injector.getInstance(INPCMove.class);
+        final Room room = new Room(map, new DoorHandler());
         room.enterRoom(player);
 		npcType = new NpcType(new Rectangle2D.Double(10,0,32,32),
                 weapon, 1, 1, "/res/playerSprite.tmx", 0, npcMove, room);
 	}
 
     @Test
-	public void testTakeDamage() throws Exception {
+	public void testTakeDamage() {
 		npcType.takeDamage(2);
-		assertTrue(npcType.getHealth()==(0));
+		assertSame(npcType.getHealth(), 0);
 	}
 
     @Test
-	public void testSetPosition() throws Exception {
+	public void testSetPosition() {
 		npcType.setPosition(new Vector2D(-1,-1));
 		assertTrue(npcType.getPosition().equals(new Vector2D(-1,-1)));
 	}
 
 
     @Test
-    public void testUpdate() throws Exception {
+    public void testUpdate() {
         assertDirectionForMovement(new Vector2D(50, 50), MoveDirection.NONE);
         assertDirectionForMovement(new Vector2D(40, 50), MoveDirection.EAST);
         assertDirectionForMovement(new Vector2D(60, 50), MoveDirection.WEST);
@@ -56,7 +58,7 @@ public class NpcTypeTest extends TestCase {
     private void assertDirectionForMovement(Vector2D npcPosition, MoveDirection direction) {
         npcType.setPosition(npcPosition);
         npcType.update();
-        assertTrue(npcType.getMoveDirection() == (direction));
+        assertSame(npcType.getMoveDirection(), direction);
     }
 }
 
