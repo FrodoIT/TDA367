29836077c56c31f3ef1712104a94f597001a8642
Ivar Josefsson <ivar_busig@hotmail.com>
2014-05-15 15:44:40 +0200

    Implemented rendermethod

--- TisButAScratch/src/main/java/scratch/view/CharacterView.java
+++ TisButAScratch/src/main/java/scratch/view/CharacterView.java
@@ -6,10 +6,17 @@
 
 package scratch.view;
 
+import java.awt.geom.Rectangle2D;
+import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.geom.Rectangle;
 import org.newdawn.slick.tiled.TiledMap;
 import scratch.model.AbstractCharacter;
+import scratch.model.IPlayerInput;
+import scratch.model.MoveDirection;
+import scratch.model.Vector2D;
 
 /**
  *
@@ -29,7 +36,17 @@ public class CharacterView {
     }
     
     public void render(GameContainer gameContainer){
-        System.out.println("We are rendering a character");
+        Vector2D position = character.getPosition();
+        Graphics graphics = gameContainer.getGraphics();
+        
+        if (character.isAttacking()) {
+            Rectangle2D.Double attackArea = character.getAttackArea();
+            graphics.setColor(Color.red);
+            graphics.fill(new Rectangle((int) attackArea.getX(), (int) attackArea.getY(), (int) attackArea.getWidth(), (int) attackArea.getHeight()));
+        }
+
+        MoveDirection input= character.getMoveDirection();
+        spriteHandler.render(graphics, input, position.getX(),position.getY());
     }
     
     
