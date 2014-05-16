ffb3d0fa538a89021bea72f0438f95ed2ec00c94
Ivar Josefsson <ivar_busig@hotmail.com>
2014-05-16 19:32:56 +0200

    Added method getClosestPlayer

--- TisButAScratch/src/main/java/scratch/construction/plugin/exported/SimpleNPCPlugin.java
+++ TisButAScratch/src/main/java/scratch/construction/plugin/exported/SimpleNPCPlugin.java
@@ -1,6 +1,5 @@
 package scratch.construction.plugin.exported;
 
-
 import com.google.inject.Inject;
 import scratch.construction.plugin.AIPlugin;
 import scratch.construction.plugin.Pluggable;
@@ -12,7 +11,8 @@ import java.awt.geom.Point2D;
  * Moves NPC toward the Player.
  */
 @AIPlugin(id = 1)
-public final class SimpleNPCPlugin implements Pluggable<SimpleNPCPlugin>, INPCMove{
+public final class SimpleNPCPlugin implements Pluggable<SimpleNPCPlugin>, INPCMove {
+
     @Inject
 
     private IRoomData roomData;
@@ -24,7 +24,7 @@ public final class SimpleNPCPlugin implements Pluggable<SimpleNPCPlugin>, INPCMo
 
     @Override
     public Vector2D calculateNewPosition(NpcType npc) {
-        final Vector2D playerPos = roomData.getPlayers().get(0).getPosition();
+        final Vector2D playerPos = getClosestPlayer(npc);
         final Vector2D npcPos = npc.getPosition();
         final Vector2D directionVector = new Vector2D(new Point2D.Double(npcPos.getX(), npcPos.getY()),
                 new Point2D.Double(playerPos.getX(), playerPos.getY())).getNormalisedVector();
@@ -33,23 +33,28 @@ public final class SimpleNPCPlugin implements Pluggable<SimpleNPCPlugin>, INPCMo
 
     }
 
-	public boolean isPromptingAnAttack(NpcType npc){
-        for(final Player player: roomData.getPlayers()){
-			if(isWithinRange(player.getPosition(), npc.getPosition())){
-				return true;
-			}
-		}
-		return false;
-	}
+    @Override
+    public boolean isPromptingAnAttack(NpcType npc) {
+        for (final Player player : roomData.getPlayers()) {
+            if (isWithinRange(player.getPosition(), npc.getPosition())) {
+                return true;
+            }
+        }
+        return false;
+    }
 
     @Override
     public void setRoomData(IRoomData roomData) {
         this.roomData = roomData;
     }
 
-    private boolean isWithinRange(Vector2D playerPos, Vector2D npcPos){
-		//+16 to get the center of the character, playerpos returns the point of the right high corner
-        final Vector2D vector2D= new  Vector2D(new Point2D.Double(playerPos.getX()+16, playerPos.getY()+16), new Point2D.Double(npcPos.getX()+16, npcPos.getY()+16));
-		return vector2D.getMagnitude()<= 48;
-	}
+    private boolean isWithinRange(Vector2D playerPos, Vector2D npcPos) {
+        //+16 to get the center of the character, playerpos returns the point of the right high corner
+        final Vector2D vector2D = new Vector2D(new Point2D.Double(playerPos.getX() + 16, playerPos.getY() + 16), new Point2D.Double(npcPos.getX() + 16, npcPos.getY() + 16));
+        return vector2D.getMagnitude() <= 48;
+    }
+
+    private Vector2D getClosestPlayer(NpcType npc) {
+        return roomData.getPlayers().get(0).getPosition();
+    }
 }
