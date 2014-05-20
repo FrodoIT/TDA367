ca2c16b39129eb6c7b9d6964526e77dd1923d32e
Ivar Josefsson <ivar_busig@hotmail.com>
2014-05-20 12:37:45 +0200

    Cleaned up code

--- TisButAScratch/src/main/java/scratch/view/RoomView.java
+++ TisButAScratch/src/main/java/scratch/view/RoomView.java
@@ -17,44 +17,11 @@ public final class RoomView {
     public RoomView(TiledMapPlus map, Room room) {
         this.map = map;
         this.room = room;
-        setupObjectImageMap();
-
-    }
-
-
-    private void setupObjectImageMap(){
 
     }
 
 
     public void render(GameContainer gameContainer) {
         map.render(0, 0);
-        /*for(final IInteractiveObject interactiveObject :  room.getInteractiveObjects()) {
-            if ( "box".compareTo(interactiveObject.getProperties().getProperty("objectType")) == 0) {
-                final String imagePath = interactiveObject.getProperties().getProperty("imagePath");
-                final Rectangle2D.Double unitTile = interactiveObject.getUnitTile();
-                final double y = unitTile.getY();
-                final double x = unitTile.getX();
-                System.out.println(x + " x " + y + " y");
-                try {
-                    SpriteDirectionRenderer spriteHandler = new SpriteDirectionRenderer(new TiledMap(imagePath));
-                    spriteHandler.render(gameContainer.getGraphics(), MoveDirection.SOUTH, x, y);
-                } catch (SlickException e){
-                    e.printStackTrace();
-
-                }
-                // graphics.drawImage(map.getTileImage((int) x, (int) y, map.getLayerIndex("background")), (float) x, (float) y) ;
-            }
-        }
-    */}
-
-    /*
-    for(Map.Entry<Integer, NpcType> npcEntry : room.getNpcs().entrySet()){
-        int id = npcEntry.getValue().getId();
-        if(npcViews.containsKey(id)){
-            npcViews.get(id).render(npcEntry.getValue(), g);
-        } else {
-            npcViews.get(0).render(npcEntry.getValue(), g);
-        }
-        */
+    }
 }
