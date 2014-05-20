ee19fb3f5b1440f3f4af437896a01402a2ed0ebb
Cannonbait <ivar_busig@hotmail.com>
2014-05-20 22:33:09 +0200

    Fixed networking

--- TisButAScratch/src/main/java/scratch/menu/Menu.java
+++ TisButAScratch/src/main/java/scratch/menu/Menu.java
@@ -1,8 +1,8 @@
- /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/*
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
 package scratch.menu;
 
 import org.newdawn.slick.AppGameContainer;
@@ -11,6 +11,7 @@ import scratch.controller.GameController;
 import scratch.model.Game;
 
 import javax.swing.*;
+import javax.swing.text.Utilities;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
@@ -22,26 +23,21 @@ public class Menu {
 
     public static void mainMenu() throws SlickException {
 
-		JFrame menu = new MainMenu();
-	    menu.setVisible(true);
+        JFrame menu = new MainMenu();
+        menu.setVisible(true);
 
     }
 
-	public static void startGame(String ip) throws SlickException {
-		Game game = new Game();
-		final org.newdawn.slick.Game controller;
-		try {
-		    InetAddress address = InetAddress.getByName(ip);
-		    if (address.isAnyLocalAddress()) {
-		        controller = new GameController(ip);
-		    } else {
-		        controller = new GameController(game);
-		    }
-		    AppGameContainer app = new AppGameContainer(controller);
-		    app.start();
-		} catch (UnknownHostException e) {
-		    e.printStackTrace();
-		}
-	}
+    public static void startGame(String ip) throws SlickException {
+        Game game = new Game();
+        final org.newdawn.slick.Game controller;
+        if (scratch.network.Utilities.validIP(ip)) {
+            controller = new GameController(ip);
+        } else {
+            controller = new GameController(game);
+        }
+        AppGameContainer app = new AppGameContainer(controller);
+        app.start();
+    }
 
 }
