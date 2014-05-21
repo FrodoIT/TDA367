0c6ad90914e7240383b5ea46311dded748dd5d50
André Samuelsson <tejpbit@hotmail.se>
2014-05-21 11:38:51 +0200

    Added packet to move character

---
+++ TisButAScratch/src/main/java/scratch/network/PacketMoveCharacter.java
@@ -0,0 +1,29 @@
+package scratch.network;
+
+/**
+ * Created by tejp on 2014-05-21.
+ */
+public class PacketMoveCharacter {
+    private int characterId, fromRoomId, toRoomId;
+
+    public PacketMoveCharacter(int characterId, int fromRoomId, int toRoomId) {
+        this.characterId = characterId;
+        this.fromRoomId = fromRoomId;
+        this.toRoomId = toRoomId;
+    }
+
+    public PacketMoveCharacter() {
+    }
+
+    public int getCharacterId() {
+        return characterId;
+    }
+
+    public int getFromRoomId() {
+        return fromRoomId;
+    }
+
+    public int getToRoomId() {
+        return toRoomId;
+    }
+}
