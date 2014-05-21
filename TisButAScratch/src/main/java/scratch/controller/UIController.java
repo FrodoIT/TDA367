268df129d687bdcd6a8b2820aa0795756e183059
André Samuelsson <tejpbit@hotmail.se>
2014-05-21 10:41:57 +0200

    removed instance of Character

--- TisButAScratch/src/main/java/scratch/controller/UIController.java
+++ TisButAScratch/src/main/java/scratch/controller/UIController.java
@@ -1,13 +1,10 @@
 package scratch.controller;
 
-import com.esotericsoftware.kryonet.Connection;
 import com.esotericsoftware.kryonet.Listener;
 import org.newdawn.slick.GameContainer;
 import scratch.model.GameCharacter;
-import scratch.view.CharacterView;
 import scratch.view.UIView;
 
-import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
 
 /**
@@ -15,25 +12,15 @@ import java.beans.PropertyChangeSupport;
  */
 public class UIController extends Listener {
 	private PropertyChangeSupport listeners;
-	private GameCharacter character;
 	private final UIView view;
 
 	public UIController(GameCharacter character) {
 		listeners = new PropertyChangeSupport(this);
-		this.character= character;
 		view = new UIView(character);
 	}
 
 	public void render(GameContainer gameContainer){
 		view.render(gameContainer);
-	}
-
-	public GameCharacter getCharacter() {
-		return character;
-	}
-
-	public int getId() {
-		return character.getId();
 	}
 
 	public UIView getView() {
