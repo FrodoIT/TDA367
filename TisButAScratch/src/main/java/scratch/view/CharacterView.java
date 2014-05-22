fcfa0fd4c0168b264301b40b88027458f18d8124
Ivar Josefsson <ivar_busig@hotmail.com>
2014-05-22 23:05:32 +0200

    Reenabled rendering of attacks

--- TisButAScratch/src/main/java/scratch/view/CharacterView.java
+++ TisButAScratch/src/main/java/scratch/view/CharacterView.java
@@ -1,16 +1,17 @@
 package scratch.view;
 
+import java.awt.geom.Rectangle2D;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.geom.Rectangle;
 import org.newdawn.slick.tiled.TiledMap;
 import scratch.model.GameCharacter;
 import scratch.model.MoveDirection;
 import scratch.model.Vector2D;
-
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 
 public class CharacterView {
 
@@ -38,12 +39,12 @@ public class CharacterView {
         }
         Vector2D position = character.getPosition();
         Graphics graphics = gameContainer.getGraphics();
-        /*
-         if (character.isPromptingAnAttack()) {
-         Rectangle2D.Double attackArea = character.getAttackArea();
-         graphics.setColor(Color.red);
-         graphics.fill(new Rectangle((int) attackArea.getX(), (int) attackArea.getY(), (int) attackArea.getWidth(), (int) attackArea.getHeight()));
-         }*/
+
+        if (character.isPromptingAnAttack()){
+            Rectangle2D.Double attackArea = character.getAttackArea();
+            graphics.setColor(Color.red);
+            graphics.fill(new Rectangle((int) attackArea.getX(), (int) attackArea.getY(), (int) attackArea.getWidth(), (int) attackArea.getHeight()));
+        }
         final MoveDirection input = character.getMoveDirection();
         spriteHandler.render(graphics, input, position.getX(), position.getY());
 
