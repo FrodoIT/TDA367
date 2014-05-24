7254d55a2960c8368df727415952e828bc5c26fc
André Samuelsson <tejpbit@hotmail.se>
2014-05-24 22:58:30 +0200

    pmd fixes for DoorHelper

--- TisButAScratch/src/main/java/scratch/model/DoorHelper.java
+++ TisButAScratch/src/main/java/scratch/model/DoorHelper.java
@@ -1,12 +1,10 @@
 package scratch.model;
 
-import scratch.model.GameCharacter;
-
 /**
  * Created by tejp on 2014-05-22.
  */
 public interface DoorHelper {
-    public boolean addCharacter(GameCharacter character);
-    public boolean removeCharacter(GameCharacter character);
-    public int getId();
+    boolean addCharacter(GameCharacter character);
+    boolean removeCharacter(GameCharacter character);
+    int getId();
 }
