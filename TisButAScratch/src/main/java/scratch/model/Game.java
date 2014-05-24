508e0cf89f2c60f99b09e6cb155c6e3a6495d944
André Samuelsson <tejpbit@hotmail.se>
2014-05-24 23:01:40 +0200

    pmd fixes for Game

--- TisButAScratch/src/main/java/scratch/model/Game.java
+++ TisButAScratch/src/main/java/scratch/model/Game.java
@@ -10,7 +10,7 @@ import java.util.List;
  */
 public class Game {
 
-    private List<GameCharacter> playerList;
+    private final List<GameCharacter> playerList;
     private List<Room> rooms;
 
     public Game() {
