ecd17fac60c4d3b5db272946cef96dac42393913
André Samuelsson <tejpbit@hotmail.se>
2014-05-24 22:58:05 +0200

    pmd fixes for Menu

--- TisButAScratch/src/main/java/scratch/menu/Menu.java
+++ TisButAScratch/src/main/java/scratch/menu/Menu.java
@@ -11,9 +11,6 @@ import scratch.controller.GameController;
 import scratch.model.Game;
 
 import javax.swing.*;
-import javax.swing.text.Utilities;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
 
 /**
  *
@@ -23,21 +20,25 @@ public class Menu {
 
     public static void mainMenu() throws SlickException {
 
-        JFrame menu = new MainMenu();
+        final JFrame menu = new MainMenu();
         menu.setVisible(true);
 
     }
 
     public static void startGame(String ip) throws SlickException {
-        Game game = new Game();
+        final Game game = new Game();
         final org.newdawn.slick.Game controller;
         if (scratch.network.Utilities.validIP(ip)) {
             controller = new GameController(ip);
         } else {
             controller = new GameController(game);
         }
-        AppGameContainer app = new AppGameContainer(controller);
+        final AppGameContainer app = new AppGameContainer(controller);
         app.start();
+    }
+
+    private Menu() {
+
     }
 
 }
